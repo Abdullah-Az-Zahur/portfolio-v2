7fdbde751c26bfa8995cfb9d771f19d05ad9214c
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-05-07 22:02:05 +0600

    wrap : layout with redux provider

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,8 +1,11 @@
+'use client'
 import Footer from "@/components/Footer/Footer";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import TabBar from "@/components/TabBar/TabBar";
 import NavBar from "@/components/NavBar/NavBar";
 import { ContextProvider } from "@/contexts/Context";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
 export default function RootLayout({
   children,
@@ -12,17 +15,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col">
-        <ContextProvider>
-          <NavBar />
-          <div className="flex flex-col md:flex-row flex-1 mt-14">
-            {<Sidebar />}
-            <main className="flex-1 overflow-y-auto pb-10 md:ml-[20%] md:pt-14">
-              {<TabBar />}
-              {children}
-            </main>
-          </div>
-          {<Footer />}
-        </ContextProvider>
+        <Provider store={store}>
+          <ContextProvider>
+            <NavBar />
+            <div className="flex flex-col md:flex-row flex-1 mt-14">
+              {<Sidebar />}
+              <main className="flex-1 overflow-y-auto pb-10 md:ml-[20%] md:pt-14">
+                {<TabBar />}
+                {children}
+              </main>
+            </div>
+            {<Footer />}
+          </ContextProvider>
+        </Provider>
       </body>
     </html>
   );
