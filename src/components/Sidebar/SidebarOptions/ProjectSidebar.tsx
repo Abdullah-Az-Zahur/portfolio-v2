f3f4c2ec272e65bfc5ff56b14f284d2207cd239f
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-05-15 00:26:02 +0600

    add : project sidebar

--- src/components/Sidebar/SidebarOptions/ProjectSidebar.tsx
+++ src/components/Sidebar/SidebarOptions/ProjectSidebar.tsx
@@ -1,8 +1,20 @@
 'use client";';
 
+import { toggleSkill } from "@/redux/features/projects/projectsSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import React, { useState } from "react";
+import { FaCss3Alt, FaHtml5, FaNodeJs, FaReact } from "react-icons/fa";
 import { IoMdArrowDropdown, IoMdArrowDropright } from "react-icons/io";
+import {
+  SiExpress,
+  SiFirebase,
+  SiJavascript,
+  SiMongodb,
+  SiNextdotjs,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
+import SkillCheckbox from "../SkillCheckbox/SkillCheckbox";
 
 const ProjectSidebar = () => {
   const dispatch = useAppDispatch();
@@ -13,10 +25,8 @@ const ProjectSidebar = () => {
     setIsDropdownOpen((prev) => !prev);
   };
 
-  const handleSkillsToggle = (skill: string) => {
-    dispatch( 
-        toggleSkill
-        (skill));
+  const handleSkillToggle = (skill: string) => {
+    dispatch(toggleSkill(skill));
   };
 
   return (
@@ -35,17 +45,125 @@ const ProjectSidebar = () => {
       {isDropdownOpen && (
         <ul className="ml-4 mt-2 space-y-1">
           {/* React.js Skill */}
-          <label
-            htmlFor="React.js"
-            className="flex items-center gap-2 cursor-pointer text-gray-500 hover:text-blue-500"
-          >
-            <input
-              type="checkbox"
-              id="React.js"
-              checked={selectedSkills.includes("React.js")}
-              onChange={() => handleSkillsToggle("React.js")}
-            />
-          </label>
+          <SkillCheckbox
+            id="React.js"
+            skillName="React.js"
+            icon={<FaReact />}
+            checked={selectedSkills.includes("React.js")}
+            onChange={() => handleSkillToggle("React.js")}
+            iconColor="text-blue-500"
+            hoverColor="text-blue-500"
+          />
+
+          {/* Next.js Skill */}
+          <SkillCheckbox
+            id="Next.js"
+            skillName="Next.js"
+            icon={<SiNextdotjs />}
+            checked={selectedSkills.includes("Next.js")}
+            onChange={() => handleSkillToggle("Next.js")}
+            iconColor="text-black"
+            hoverColor="text-black"
+          />
+
+          {/* Node.js Skill */}
+          <SkillCheckbox
+            id="Node.js"
+            skillName="Node.js"
+            icon={<FaNodeJs />}
+            checked={selectedSkills.includes("Node.js")}
+            onChange={() => handleSkillToggle("Node.js")}
+            iconColor="text-green-600"
+            hoverColor="text-green-600"
+          />
+
+          {/* Express.js Skill */}
+          <SkillCheckbox
+            id="Express.js"
+            skillName="Express.js"
+            icon={<SiExpress />}
+            checked={selectedSkills.includes("Express.js")}
+            onChange={() => handleSkillToggle("Express.js")}
+            iconColor="text-gray-700"
+            hoverColor="text-gray-700"
+          />
+
+          {/* MongoDB Skill */}
+          <SkillCheckbox
+            id="MongoDB"
+            skillName="MongoDB"
+            icon={<SiMongodb />}
+            checked={selectedSkills.includes("MongoDB")}
+            onChange={() => handleSkillToggle("MongoDB")}
+            iconColor="text-green-500"
+            hoverColor="text-green-500"
+          />
+
+          {/* Firebase Skill */}
+          <SkillCheckbox
+            id="Firebase"
+            skillName="Firebase"
+            icon={<SiFirebase />}
+            checked={selectedSkills.includes("Firebase")}
+            onChange={() => handleSkillToggle("Firebase")}
+            iconColor="text-yellow-500"
+            hoverColor="text-yellow-500"
+          />
+
+          {/* TypeScript Skill */}
+          <SkillCheckbox
+            id="TypeScript"
+            skillName="TypeScript"
+            icon={<SiTypescript />}
+            checked={selectedSkills.includes("TypeScript")}
+            onChange={() => handleSkillToggle("TypeScript")}
+            iconColor="text-blue-600"
+            hoverColor="text-blue-600"
+          />
+
+          {/* JavaScript Skill */}
+          <SkillCheckbox
+            id="JavaScript"
+            skillName="JavaScript"
+            icon={<SiJavascript />}
+            checked={selectedSkills.includes("JavaScript")}
+            onChange={() => handleSkillToggle("JavaScript")}
+            iconColor="text-yellow-400"
+            hoverColor="text-yellow-400"
+          />
+
+          {/* TailwindCSS Skill */}
+          <SkillCheckbox
+            id="TailwindCSS"
+            skillName="TailwindCSS"
+            icon={<SiTailwindcss />}
+            checked={selectedSkills.includes("TailwindCSS")}
+            onChange={() => handleSkillToggle("TailwindCSS")}
+            iconColor="text-blue-400"
+            hoverColor="text-blue-400"
+          />
+
+          {/* HTML Skill */}
+          <SkillCheckbox
+            id="HTML"
+            skillName="HTML"
+            icon={<FaHtml5 />}
+            checked={selectedSkills.includes("HTML")}
+            onChange={() => handleSkillToggle("HTML")}
+            iconColor="text-orange-500"
+            hoverColor="text-orange-500"
+          />
+
+          {/* CSS Skill */}
+          <SkillCheckbox
+            id="CSS"
+            skillName="CSS"
+            icon={<FaCss3Alt />}
+            checked={selectedSkills.includes("CSS")}
+            onChange={() => handleSkillToggle("CSS")}
+            iconColor="text-blue-300"
+            hoverColor="text-blue-300"
+          />
         </ul>
       )}
     </>
---
+++ src/components/Sidebar/SkillCheckbox/SkillCheckbox.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+interface SkillCheckboxProps {
+  id: string;
+  skillName: string;
+  icon: React.ReactNode;
+  checked: boolean;
+  onChange: () => void;
+  iconColor: string;
+  hoverColor: string;
+}
+
+const SkillCheckbox: React.FC<SkillCheckboxProps> = ({
+  id,
+  skillName,
+  icon,
+  checked,
+  onChange,
+  iconColor,
+  hoverColor,
+}) => {
+  return (
+    <label
+      htmlFor={id}
+      className={`flex items-center gap-2 cursor-pointer text-gray-500 hover:${hoverColor}`}
+    >
+      <input
+        type="checkbox"
+        id={id}
+        checked={checked}
+        onChange={onChange}
+        className="mr-2"
+      />
+      <span className={`${iconColor}`}>{icon}</span>
+      <span>{skillName}</span>
+    </label>
+  );
+};
+
+export default SkillCheckbox;
