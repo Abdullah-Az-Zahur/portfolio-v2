b8b2a2ab2c09fc8731846c425d597c35c495bd2d
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-05-15 20:21:14 +0600

    fix : errors

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import TypingAnimation from "@/components/TypingAnimation/TypingAnimation";
 import Image from "next/image";
 import Link from "next/link";
--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,13 +1,13 @@
 // app/api/send/route.ts
-import { NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
 export async function POST(request: Request) {
   const { name, email, message } = await request.json();
 
   // Create a transporter
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
       user: process.env.NEXT_PUBLIC_EMAIL_USER,
       pass: process.env.NEXT_PUBLIC_EMAIL_PASS,
@@ -17,7 +17,7 @@ export async function POST(request: Request) {
   // Email options
   const mailOptions = {
     from: email,
-    to: 'abdullah.az.zahur@gmail.com',
+    to: "abdullah.az.zahur@gmail.com",
     subject: `New message from ${name}`,
     text: message,
     html: `
